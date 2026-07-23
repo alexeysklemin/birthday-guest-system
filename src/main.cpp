e74c7d1801008e97aa8ea09afeb44decabee369d
Alex Sklemin <a76sklemin@gmail.com>
2026-07-23 21:39:21 +0400

    combo datas

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,47 @@
 #include "Guests.pb.h"  // Сгенерированный файл (лежит в build/)
 #include <iostream>
-
+#include <locale>
 
 
 int main()
 {
 	GOOGLE_PROTOBUF_VERIFY_VERSION;
+	setlocale(LC_ALL, "Russian");
 	
 	birthday::Guest guest;  // birthday — это package из .proto
 	guest.set_name("Михаил");
 	guest.set_age(30);
-	guest.set_city("Уфа");
 	
-	std::cout << "Имя: " << guest.name() << "\n";
-	std::cout << "Возраст: " << guest.age() << "\n";
-	std::cout << "Город: " << guest.city() << "\n";
+    guest.add_gifts("Книга");
+    guest.add_gifts("Часы");
+    guest.add_gifts("Кошелёк");
 	
+	if (!guest.has_email()) {
+      std::cout << "Email не задан\n";
+    }
+	
+	birthday::Address addr;
+    addr.set_city("Уфа");
+    addr.set_street("Ленина");
+    addr.set_homenumber(42);
+	
+	// Устанавливаем адрес в guest
+    *guest.mutable_address() = addr;
+	
+	if (guest.has_email()) {
+		std::cout << "Email: " << guest.email() << "\n";
+    }
+    
+    // Адрес
+    if (guest.has_address()) {
+        std::cout << "Адрес: " << guest.address().city() << ", " 
+                  << guest.address().street() << ", д. " 
+                  << guest.address().homenumber() << "\n";
+    }
+	
+    std::cout << "\nПодарки:\n";
+    for (int i = 0; i < guest.gifts_size(); ++i) {
+        std::cout << "  " << i + 1 << ". " << guest.gifts(i) << "\n";
+    }
 	google::protobuf::ShutdownProtobufLibrary();
 }
